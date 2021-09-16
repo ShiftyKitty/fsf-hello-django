4301c6fb4e9368b498ac8b934e1a1eb26ea735f8
ShiftyKitty <cathalpmcgahan@gmail.com>
2021-09-16 10:15:34 +0000

    fixed allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-h)k4=o!hg3tpab^(u2mz3k!17ligob7xkr(fl+o(hjk90x(v6s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shiftykitty-django-todo-app.herokuapp.com']
 
 
 # Application definition
